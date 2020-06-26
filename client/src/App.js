159c4d37678a79f67cbf8c16e8959cbb394a7435
Anku786 <bishnoiankita23@gmail.com>
2020-06-25 19:29:08 -0700

    Dispatched user data from signin

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,48 @@
-import React from 'react';
+import React , {useEffect , createContext , useReducer} from 'react';
 import "./App.css";
 import NavBar from './components/Navbar';
-import {BrowserRouter ,  Route} from 'react-router-dom';
+import {BrowserRouter ,  Route , Switch , useHistory} from 'react-router-dom';
 import Home from './components/screens/Home';
 import Signin from './components/screens/Signin';
 import Profile from './components/screens/Profile';
 import Signup from './components/screens/Signup';
 import CreatePost from './components/screens/CreatePost';
+import {reducer , initialState} from './reducers/userReducer';
 
+export const UserContext = createContext()
 
+const Routing = () =>{
+  const history = useHistory()
+  return(
+    <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/signin">
+          <Signin />
+        </Route>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+        <Route path="/profile">
+          <Profile />
+        </Route>
+        <Route path="/create">
+          <CreatePost />
+        </Route>
+    </Switch>
+    );
+}
 
 function App() {
+  const [state,dispatch] = useReducer(reducer,initialState)
   return (
-    <BrowserRouter>
-      <NavBar />
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route path="/signin">
-        <Signin />
-      </Route>
-      <Route path="/signup">
-        <Signup />
-      </Route>
-      <Route path="/profile">
-        <Profile />
-      </Route>
-      <Route path="/create">
-        <CreatePost />
-      </Route>
-    </BrowserRouter>
+    <UserContext.Provider value={{state:state , dispatch:dispatch}}>
+      <BrowserRouter>
+        <NavBar />
+        <Routing />
+      </BrowserRouter>
+    </UserContext.Provider>
   );
 }
 
